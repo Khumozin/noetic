6eed111c533c3f90a7d6e74965c3f3cc661f5eda
Khumo Mogorosi <khumomogorosi@gmail.com>
2026-09-05 21:10:31 +0200

    chore: add @vitest/coverage-v8

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['src/**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './coverage',
+
+      thresholds: {
+        statements: 70,
+        branches: 60,
+        functions: 60,
+        lines: 70,
+      },
+    },
+  },
+});
